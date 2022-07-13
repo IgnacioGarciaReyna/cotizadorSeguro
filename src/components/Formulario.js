ad577bebb9e3c24e59a7a79fe417b2535f571353
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-07-13 18:07:38 -0300

    Se agregó estilo al botón del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -23,6 +23,25 @@ const InputRadio = styled.input`
   margin: 0 1rem;
 `;
 
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color .3s ease;
+  margin-top: 2rem;
+
+  //Sintaxis de sass
+  &:hover {
+    background-color: #26c6da;
+    cursor: pointer;
+  }
+`;
+
 const Formulario = () => {
   return (
     <form>
@@ -57,7 +76,7 @@ const Formulario = () => {
         <InputRadio type="radio" name="plan" value="completo" /> Completo
       </Campo>
 
-      <button type="button">Cotizar</button>
+      <Boton type="button">Cotizar</Boton>
     </form>
   );
 };
