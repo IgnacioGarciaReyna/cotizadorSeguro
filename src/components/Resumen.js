c97da30ff9c73e72614bb9a742f8034bd5c579bd
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-07-31 23:08:45 -0300

    Se dió estilo al resumen de cotización

--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,4 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
+import styled from "@emotion/styled";
+import { primerMayuscula } from "../helper";
+
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838f;
+    color: #fff;
+    margin-top: 1rem;
+`
 
 const Resumen = ({ datos }) => {
   //extraer de datos(para no estar escribiendo datos.marca)
@@ -7,14 +18,14 @@ const Resumen = ({ datos }) => {
   if (marca === "" || year === "" || plan === "") return null;
 
   return (
-    <Fragment>
+    <ContenedorResumen>
       <h2>Resumen de Cotización</h2>
       <ul>
-        <li>Marca: </li>
-        <li>Plan: </li>
-        <li>Año del auto: </li>
+        <li>Marca: {primerMayuscula(marca)}</li>
+        <li>Plan: {primerMayuscula(plan)} </li>
+        <li>Año del auto: {year} </li>
       </ul>
-    </Fragment>
+    </ContenedorResumen>
   );
 };
 
