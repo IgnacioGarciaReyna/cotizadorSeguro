e10e67774e17b8ce44b77bde458b880e73769603
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-07-31 23:37:19 -0300

    Se agregaron TransitionGroup y CSSTransition al resultado

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "@emotion/styled";
 
+//Se importaron para transisiones sin necesidad de usar css. Agregan valores al index.css
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 const Mensaje = styled.p`
   background-color: rgb(127, 224, 237);
   margin-top: 2rem;
@@ -30,7 +33,15 @@ const Resultado = ({ cotizacion }) => {
     <Mensaje>Elige marca, año y tipo de seguro</Mensaje>
   ) : (
     <ResultadoCotizacion>
-      <TextoCotizacion>El total es: $ {cotizacion}</TextoCotizacion>
+      <TransitionGroup component="p" className="resultado">
+        <CSSTransition
+          classNames="resultado"
+          key={cotizacion}
+          timeout={{ enter: 500, exit: 500 }}
+        >
+          <TextoCotizacion>El total es: $ {cotizacion}</TextoCotizacion>
+        </CSSTransition>
+      </TransitionGroup>
     </ResultadoCotizacion>
   );
 };
